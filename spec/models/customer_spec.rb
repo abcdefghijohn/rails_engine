a25699d24a24805b0213248e6e14fcc97ac99dc9
John Kim <abcdefghijohnk@gmail.com>
2020-12-12 19:05:13 -0700

    Write model spec for customer, pass

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it {should validate_presence_of :first_name}
+    it {should validate_presence_of :last_name}
+    it {should validate_presence_of :created_at}
+    it {should validate_presence_of :updated_at}
+  end
+
+  describe 'relationships' do
+    it {should have_many :invoices}
+  end
 end
