aa9de08391df309c130444d8a6104a1a5036a7ba
John Kim <abcdefghijohnk@gmail.com>
2020-12-12 19:50:23 -0700

    Create transaction model and spec

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,5 @@
 class Transaction < ApplicationRecord
-  belongs_to :invoice, dependent: :destroy
+  validates :invoice_id, :credit_card_number, :credit_card_expiration_date, :result, :created_at, :updated_at, presence: true
+
+  belongs_to :invoice
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it {should validate_presence_of :invoice_id}
+    it {should validate_presence_of :credit_card_number}
+    it {should validate_presence_of :credit_card_expiration_date}
+    it {should validate_presence_of :result}
+    it {should validate_presence_of :created_at}
+    it {should validate_presence_of :updated_at}
+  end
+
+  describe 'relationships' do
+    it {should belong_to :invoice}
+  end
 end
