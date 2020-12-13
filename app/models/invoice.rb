9b1bc767fb8bdafbcc072c4a21affb0d6c6c91b4
John Kim <abcdefghijohnk@gmail.com>
2020-12-12 19:49:36 -0700

    Create invoice model and spec

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,9 @@
 class Invoice < ApplicationRecord
-  belongs_to :customer, dependent: :destroy
-  belongs_to :merchant, dependent: :destroy
+  validates :customer_id, :merchant_id, :status, :created_at, :updated_at, presence: true
+
+  belongs_to :customer
+  belongs_to :merchant
+
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it {should validate_presence_of :customer_id}
+    it {should validate_presence_of :merchant_id}
+    it {should validate_presence_of :status}
+    it {should validate_presence_of :created_at}
+    it {should validate_presence_of :updated_at}
+  end
+
+  describe 'relationships' do
+    it {should belong_to :customer}
+    it {should belong_to :merchant}
+    it {should have_many :invoice_items}
+    it {should have_many(:items).through(:invoice_items)}
+  end
 end
