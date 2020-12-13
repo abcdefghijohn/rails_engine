beaca914a0419ff2f5051e40bf874bc826c0c417
John Kim <abcdefghijohnk@gmail.com>
2020-12-12 19:50:00 -0700

    Create item model and spec

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
-  belongs_to :merchant, dependent: :destroy
+  validates :name, :description, :unit_price, :merchant_id, :created_at, :updated_at, presence: true
+
+  belongs_to :merchant
+  has_many :invoice_items
+  has_many :invoices, through: :invoice_items
 end
