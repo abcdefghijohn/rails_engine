902df8a7630c51d1c92de85c8cc237ddcc71b4b8
John Kim <abcdefghijohnk@gmail.com>
2020-12-14 19:45:39 -0700

    Update merchants controller with show action

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -2,4 +2,9 @@ class Api::V1::MerchantsController < ApplicationController
   def index
     render json: MerchantSerializer.new(Merchant.all)
   end
+
+  def show
+    id = params[:id]
+    render json: MerchantSerializer.new(Merchant.find_by(id: id))
+  end
 end
