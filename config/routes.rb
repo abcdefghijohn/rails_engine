302e4f569af4a080fb8a3e27a6d1d4ec76c5411f
John Kim <abcdefghijohnk@gmail.com>
2020-12-14 19:45:25 -0700

    Add merchant show to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: %i[index]
+      resources :merchants, only: %i[index show]
     end
   end
 end
