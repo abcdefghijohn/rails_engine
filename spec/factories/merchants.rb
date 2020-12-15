a97aafe87514a1678094bb6d0b5397f550c7f447
John Kim <abcdefghijohnk@gmail.com>
2020-12-14 18:37:10 -0700

    Create merchants request spec and index route

--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :merchant do
-    name { "MyString" }
+    name { Faker::Company.name }
+    created_at { "2020-12-14 17:42:10" }
+    updated_at { "2020-12-14 17:42:10" }
   end
 end
