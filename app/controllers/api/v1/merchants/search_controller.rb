50750fcf471a8f2441354c2190fb716e3cc9eb96
John Kim <abcdefghijohnk@gmail.com>
2020-12-16 21:59:59 -0700

    Create find_all action in search_controller

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -8,6 +8,12 @@ module Api
           render json: MerchantSerializer.new(merchant)
         end
 
+        def find_all
+          like_keyword = "%#{merchant_params[:name]}%"
+          merchants = Merchant.where('name ILIKE ?', like_keyword)
+          render json: MerchantSerializer.new(merchants)
+        end
+
         private
         def merchant_params
           params.permit(:name, :created_at, :updated_at)
